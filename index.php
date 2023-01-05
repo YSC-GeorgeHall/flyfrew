1c5dbbf915f1e640fdb1a12347a2d66b62980dd4
George Hall <georgehall@Georges-MacBook-Air.local>
2023-01-05 15:01:38 +0000

    Added cards for each option

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fly Frew</title>
+    <script src="https://kit.fontawesome.com/ea02caf681.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,500&display=swap" rel="stylesheet">
     <style> @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
@@ -16,9 +17,10 @@
 
   </head>
   <body>
+
   <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">FlyFrew</a>
+    <a class="navbar-brand" href="index.php">FlyFrew</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -28,13 +30,17 @@
       <div class="d flex justify-content-end">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">About Us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Partners</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link">News</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link">Get In Touch</a>
         </li>
       </ul>
     </div>
@@ -43,7 +49,7 @@
 </nav>
 <!-- start of container--><div class="container">
 
-<div class="jumbotron">
+<div class="jumbotron" style="padding-bottom: 15px;">
   <div class="row flex align-items-center">
     
     <div class="col-md-6">
@@ -52,10 +58,7 @@
         <div class="col-md-12">
         <h4>The future of the travel is a connected and fully integrated experience, giving travellers ultimate choice and flexibility</h4></br>
         <small>See our list of products that can enhance your travellers experiences and increase your revenue growth</small></br>
-        <button type="button" class="btn btn-outline-primary">FlyFrew Airport</button>
-        <button type="button" class="btn btn-outline-primary">FlyFrew Lounge</button>
-        <button type="button" class="btn btn-outline-primary">FlyFrew Corporate</button>
-        <button type="button" class="btn btn-outline-primary">FlyFrew Transfers</button>
+
 </div>  
 </div>
 
@@ -66,14 +69,65 @@
   </div>
 </div>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="images/airport.png" class="img-fluid" alt="Responsive image" alt="Card image cap">
+<div class="row">
+  <div class="col-md-3">
+<div class="card">
+  <img class="card-img-top" src="images/airport.png" class="img-fluid" alt="Responsive image" alt="Card image cap" style="height: 125px;">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">FlyFrew Airport</h5>
+    <p class="card-text">Seemless and integrated experiences offering travellers the ultimate airport experience, at their finger tips</p>
+    <button type="button" class="btn btn-outline-primary">Learn more about FlyFrew Airport</button>
+
   </div>
 </div>
+  </div>
+  <div class="col-md-3">
+<div class="card">
+  <img class="card-img-top" src="images/lounge.png" class="img-fluid" alt="Responsive image" alt="Card image cap" style="height: 125px;">
+  <div class="card-body">
+    <h5 class="card-title">FlyFrew Lounge</h5>
+    <p class="card-text">Provide travllers with the ultimate in-lounge experience, giving them everything they need in the palm of their hands</p>
+    <button type="button" class="btn btn-outline-primary">Learn more about FlyFrew Lounge</button>
+
+  </div>
+</div>
+  </div>
+  <div class="col-md-3">
+<div class="card">
+  <img class="card-img-top" src="images/transfer.png" class="img-fluid" alt="Responsive image" alt="Card image cap" style="height: 125px;">
+  <div class="card-body">
+    <h5 class="card-title">FlyFrew Transfers</h5>
+    <p class="card-text">Travelling doesnt start at the airport, it starts at home, give your travellers the ultimate choice in getting to the airport</p>
+
+        <button type="button" class="btn btn-outline-primary">Learn more about FlyFrew Transfers</button>
+  </div>
+</div>
+  </div>
+  <div class="col-md-3">
+<div class="card">
+  <img class="card-img-top" src="images/corporate.png" class="img-fluid" alt="Responsive image" alt="Card image cap" style="height: 125px;">
+  <div class="card-body">
+    <h5 class="card-title">FlyFrew Corporate</h5>
+    <p class="card-text">Corporate travellers are vital to an airport ecosystem. Offer the corporate traveller the complete booking tool, from start to finish</p>
+    <button type="button" class="btn btn-outline-primary">Learn more about FlyFrew Corporate</button>
+  </div>
+</div>
+  </div>
+</div>
+
+<!-- Footer Start -->
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    
+
+    <ul class="nav col-md-4 justify-content-start">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About Us</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">News</a></li>
+    </ul>
+
+</br>
+<a href ="https://www.w3schools.com">   <i class="fa-brands fa-facebook fa-2xl " style="padding-right: 5px;"></i> </a>
+<a href ="https://www.w3schools.com">     <i class="fa-brands fa-twitter fa-2xl" style="padding-right: 5px;"></i></a>
+  <!-- Footer End --></footer>
 
 <!-- end of container--></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
