54e622dba5f7caade290e198caae25e60aff40f5
George Hall <georgehall@Georges-MacBook-Air.local>
2023-01-05 13:43:06 +0000

    Added Navbar and Jumbotron with styling and a google font.

--- index.php
+++ index.php
@@ -1,6 +1,67 @@
-<html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fly Frew</title>
-<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,500&display=swap" rel="stylesheet">
+    <style> @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
+  body  {
+    font-family: 'Rubik', sans-serif;
 
-</head>
+  }
+</style>
+
+
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">FlyFrew</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <div class="d flex justify-content-end">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </div>
+</nav>
+<!-- start of container--><div class="container">
+
+<div class="jumbotron">
+  <div class="row flex align-items-center">
+    
+    <div class="col-md-6">
+    <h1 style="text-align: center;">A Suite airport products</h1>
+        <div class="row">
+        <div class="col-md-12">
+        <h4>The future of the airport is a connected and fully integrated experience, offering travellers a full range of services digitally. </h4>
+</div>  
+</div>
+
+    </div>
+    <div class="col-md-6">
+      <img src="images/banner1.png" class="img-fluid" alt="Responsive image">
+    </div>
+  </div>
+</div>
+
+<!-- end of container--></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
 </html>
